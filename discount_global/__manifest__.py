05e1cd5ea3a8049c25bb832f84c6278805867858
Adan <adanasr@gmail.com>
2021-06-21 12:10:13 -0600

    Global Discount

---
+++ discount_global/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'Global Discount by J8C',
+    'version': '18.06.2021',
+    'summary': """Sales Global Discount""",
+    'description': 'Sales Global Discount',
+    "category": "Sale",
+    "author": "J8C",
+    'depends': ['sale'],
+    'data': [
+        'views/sale_views.xml',
+    ],
+    'license': 'AGPL-3',
+    'demo': [],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
