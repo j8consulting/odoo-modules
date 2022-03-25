20485b770380c6d7c0894bc8ff247ddaa669e0f3
Adan <adanasr@gmail.com>
2022-03-25 12:12:04 -0600

    Nuevos modulos

---
+++ impuesto_dinamico_sale_purchase/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'Default Taxes',
+    'version': '15.02.2022',
+    'summary': """Default Taxes by J8C""",
+    'description': 'Default Taxes by J8C',
+    "category": "Sale",
+    "author": "Soy Adan Najera J8C",
+    'depends': ['sale', 'base'],
+    'data': [
+        'views/contacto_impuesto_view.xml',
+    ],
+    'license': 'AGPL-3',
+    'demo': [],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
---
+++ sale_purchase_amount_to_text/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'Sale and Purchase Total Amount to Text By J8C',
+    'version': '22.03.2022',
+    'summary': """Sale and Purchase Total Amount to Text By J8C""",
+    'description': 'Sale and Purchase Total Amount to Text By J8C by J8C',
+    "category": "Sale",
+    "author": "J8C",
+    'depends': ['sale', 'purchase'],
+    'data': [
+        'views/sale_views.xml',
+    ],
+    'license': 'AGPL-3',
+    'demo': [],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
